9604d1fe9902d8d68595c9334d927b8d2e6a7a9e
bermudo <marl.aldwin.bermudo@gmail.com>
2019-02-14 09:32:40 +0800

    New Feature: First Deploy Description: Deployed the basic react page

---
+++ precache-manifest.31449054393cd8074fd10d86849002db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e7840a0ca202425776c9",
+    "url": "/static/css/main.b402e1d3.chunk.css"
+  },
+  {
+    "revision": "e7840a0ca202425776c9",
+    "url": "/static/js/main.e7840a0c.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "123bd6c507ea123ce21f",
+    "url": "/static/js/2.123bd6c5.chunk.js"
+  },
+  {
+    "revision": "3e0bb9d63519c0f4ca109cc8cf32c6c7",
+    "url": "/static/media/Marl-Bermudo-OJ-Abesamis-thesis.3e0bb9d6.pdf"
+  },
+  {
+    "revision": "8949348d8a9d55c18b35a60e72c7c96d",
+    "url": "/index.html"
+  }
+];
